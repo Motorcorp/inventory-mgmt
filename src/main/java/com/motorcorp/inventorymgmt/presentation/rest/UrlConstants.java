3324cd74ce8a0eab49d3c5cca5ce27deea734627
Varun <varunkothapally18@gmail.com>
2025-09-03 19:46:57 -0500

    Added UrlConstants.java file containing constants related to project

---
+++ src/main/java/com/motorcorp/inventorymgmt/presentation/rest/UrlConstants.java
@@ -0,0 +1,8 @@
+package com.motorcorp.inventorymgmt.presentation.rest;
+
+public class UrlConstants {
+
+    public static final String BASE_URL ="/api/inventory";
+    public static final String ITEM_NOT_FOUND = "item.notFound";
+    public static final String REQUEST_STATUS_FAILED= "Failed";
+}
